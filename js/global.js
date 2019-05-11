fd5ccf1f36f17b2f8bba9707664b2868c5a436ad
Matti Liikala <matti.liikala@sofokus.com>
2019-05-11 22:30:17 +0300

    footer content, styling

---
+++ js/global.js
@@ -0,0 +1,13 @@
+/**
+ * Theme global.js
+ */
+
+function init()
+{
+    'use strict';
+
+    //Initialize Animate on scroll script
+    AOS.init();
+}
+
+document.addEventListener('DOMContentLoaded', init);
